fc0e4e44edab3dd3af76e0f9b39d61c6b1f09898
Daniel Mabadeje <mabadejedaniel1@gmail.com>
2023-09-20 20:05:14 +0100

    Added paystack confirmation

--- web/modules/custom/kindlegatedonation/src/Controller/DonationApiController.php
+++ web/modules/custom/kindlegatedonation/src/Controller/DonationApiController.php
@@ -5,8 +5,10 @@ namespace Drupal\kindlegatedonation\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
-class DonationApiController extends ControllerBase {
+class DonationApiController extends ControllerBase
+{
 
   /**
    * Donation API endpoint.
@@ -17,10 +19,11 @@ class DonationApiController extends ControllerBase {
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    *   The JSON response.
    */
-  public function donate(Request $request) {
+  public function donate(Request $request)
+  {
     // Extract donation data from the JSON request.
     $data = json_decode($request->getContent(), TRUE);
-  
+
     // Create a new donation node.
     $node = Node::create([
       'type' => 'donation', // Use the machine name of your donation content type.
@@ -30,27 +33,53 @@ class DonationApiController extends ControllerBase {
       'field_amount' => $data['amount'],
       // Add other fields as needed.
     ]);
-  
+
     // Save the donation node.
     $node->save();
-  
+
     // Example response.
     $response = [
       'status' => 'success',
       'message' => 'Donation processed and recorded successfully.',
     ];
-  
+
     return new JsonResponse($response);
   }
-  
+
 
   public function confirmStripe()
   {
-    
   }
 
-  public function confirmPaystack()
+  public function confirmPaystack($reference)
   {
-    
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.paystack.co/transaction/verify/".$reference,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer SECRET_KEY",
+        "Cache-Control: no-cache",
+      ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+      echo "cURL Error #:" . $err;
+      return false;
+    } else {
+      return $response;
+    }
   }
 }
